a55865660d600edbd3806bd1281e7fbda48ffd14
Yonder <jon.xjonx.nat@gmail.com>
2022-08-11 15:33:22 -0400

    Update README, help output, and the compile script
    Update `README.md`, `compile.h`, and `bnrtool.c`'s help output string.

--- compile.sh
+++ compile.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 ASAN=""
 OUTEXT=""
-if [ $# -gt 0 ]; then
-    if [ "$1" == "ASAN" ]; then
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    OUTEXT=".exe"
+elif [ $# -gt 0 ]; then
+    if [ "$1" == "asan" ]; then
         ASAN="-fsanitize=address -g"
     fi
 fi
-if [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    OUTEXT=".exe"
-fi
 if [ ! -d "./dst" ]; then mkdir ./dst ; fi
-gcc -std=c99 $ASAN -Wno-address-of-packed-member -Wno-stringop-overflow -Wvla -o ./dst/bnrtool$OUTEXT ./src/bnrtool.c ./src/tga.c ./src/gxtexture.c ./src/strsep.c ./src/cgetchar.c
+gcc -std=c99 $ASAN -Wno-address-of-packed-member -Wno-stringop-overflow -Wvla -o ./dst/bnrtool$OUTEXT ./src/bnrtool.c \
+    ./src/tga.c \
+    ./src/gxtexture.c \
+    ./src/strsep.c \
+    ./src/cgetchar.c
 md5sum -b ./dst/bnrtool$OUTEXT > ./dst/bnrtool$OUTEXT.md5
 sha1sum -b ./dst/bnrtool$OUTEXT > ./dst/bnrtool$OUTEXT.sha1
 sha256sum -b ./dst/bnrtool$OUTEXT > ./dst/bnrtool$OUTEXT.sha256
