79daadd71bc91b86a43f50121b7ee98c9366ae95
Yonder <jon.xjonx.nat@gmail.com>
2024-12-20 07:17:42 -0500

    Fix building on linux

--- package.sh
+++ package.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 dp0="$(dirname $(readlink -m $BASH_SOURCE))"
 destdir="$(printf '%s' $1 | sed -E -e 's/\\/\//g' -e 's/C:/\/c/g' -e 's/\"*([^\"]+)\"*/\1/')"
+iswin=""
+[ -n "${4}" ] && iswin="1"
+isfile=""
+if [ -n "${4}" ] && [ "${4}" == *dll ] ; then
+    isfile="1"
+fi
+[ -n "${iswin}" ] && lext=".dll" || lext=".so"
+[ -n "${iswin}" ] && eext=".exe" || eext=""
 echo "Packaging from publish dir ${destdir}..."
-if [ -d "${destdir}" ] && [ -f "${destdir}/bnrtool.exe" ] && ( [ -n "${4}" ] || [ -f "${destdir}/libc++.dll" ] ) && [ -f "${destdir}/libgxtexture.dll" ] ; then
+if [ -d "${destdir}" ] && [ -f "${destdir}/bnrtool${eext}" ] && ( [ -z "${isfile}" ] || [ -f "${destdir}/${4}" ] ) && [ -f "${destdir}/libgxtexture${lext}" ] ; then
         mkdir -p "${destdir}/package/bnrtool"
         mkdir -p "${destdir}/package/bnrtool/licenses"
-        cp -f "${destdir}/bnrtool.exe" "${destdir}/package/bnrtool/bnrtool.exe"
-        [ -n "${4}" ] && cp -f "${destdir}/libc++.dll" "${destdir}/package/bnrtool/libc++.dll"
-        cp -f "${destdir}/libgxtexture.dll" "${destdir}/package/bnrtool/libgxtexture.dll"
-        "$dp0/hash.sh" "${destdir}/package/bnrtool/bnrtool.exe"
-        [ -n "${4}" ] && "$dp0/hash.sh" "${destdir}/package/bnrtool/libc++.dll"
-        "$dp0/hash.sh" "${destdir}/package/bnrtool/libgxtexture.dll"
-        [ -n "${4}" ] && "$dp0/libcver.sh" "${destdir}/package/bnrtool"
+        cp -f "${destdir}/bnrtool${eext}" "${destdir}/package/bnrtool/bnrtool${eext}"
+        [ -n "${isfile}" ] && cp -f "${destdir}/${4}" "${destdir}/package/bnrtool/${4}"
+        cp -f "${destdir}/libgxtexture${lext}" "${destdir}/package/bnrtool/libgxtexture${lext}"
+        "$dp0/hash.sh" "${destdir}/package/bnrtool/bnrtool${eext}"
+        [ -n "${isfile}" ] && "$dp0/hash.sh" "${destdir}/package/bnrtool/${4}"
+        "$dp0/hash.sh" "${destdir}/package/bnrtool/libgxtexture${lext}"
+        [ -n "${iswin}" ] && "$dp0/libcver.sh" "${destdir}/package/bnrtool"
         cp -f "$dp0/LICENSE" "${destdir}/package/bnrtool/LICENSE"
         cp -f "$dp0/README.md" "${destdir}/package/bnrtool/README.md"
         cp -f -R "$dp0/licenses/." "${destdir}/package/bnrtool/licenses"
@@ -25,11 +33,11 @@ if [ -d "${destdir}" ] && [ -f "${destdir}/bnrtool.exe" ] && ( [ -n "${4}" ] || 
         return 0 2>/dev/null
         exit 0
 else
-    [ -f "${destdir}/bnrtool.exe" ] || echo "${destdir}/bnrtool.exe doesnt exist; packaging failed"
-    if [ -n "${4}" ] && [ -f "${destdir}/libc++.dll" ] ; then
-        echo "${destdir}/libc++.dll doesnt exist; packaging failed"
+    [ -f "${destdir}/bnrtool${eext}" ] || echo "${destdir}/bnrtool${eext} doesnt exist; packaging failed"
+    if [ -n "${isfile}" ] && [ -f "${destdir}/${4}" ] ; then
+        echo "${destdir}/${4} doesnt exist; packaging failed"
     fi
-    [ -f "${destdir}/libgxtexture.dll" ] || echo "${destdir}/libgxtexture.dll doesnt exist; packaging failed"
+    [ -f "${destdir}/libgxtexture${lext}" ] || echo "${destdir}/libgxtexture${lext} doesnt exist; packaging failed"
     [ -d "${destdir}" ] || echo "${destdir} doesnt exist; packaging failed"
     return 1 2>/dev/null
     exit 1
